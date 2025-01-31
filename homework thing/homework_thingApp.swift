e32e583a7f52e20dace7615747ac4b2cfb247b5d
lucuinhas <lucarvalhodealmeida@gmail.com>
2025-01-30 20:03:00 -0400

    Initial Commit

---
+++ homework thing/homework_thingApp.swift
@@ -0,0 +1,32 @@
+//
+//  homework_thingApp.swift
+//  homework thing
+//
+//  Created by lucas on 30/01/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct homework_thingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
